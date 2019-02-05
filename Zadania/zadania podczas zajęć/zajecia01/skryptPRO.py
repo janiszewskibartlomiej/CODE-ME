3b2ab1ada5d6c5068a256032eacd9c0ec08a6818
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2019-02-06 00:15:33 +0100

    dodanie pierwszych zajec

---
+++ Zadania/zadania podczas zajęć/zajecia01/skryptPRO.py
@@ -0,0 +1,17 @@
+cena_gazu = 2.08
+dystans = int(input('Podaj przejechany dystans:'))
+
+if dystans is not 0:
+    spalanie_na100km = 12.8
+    koszt = spalanie_na100km * dystans / 100 * cena_gazu
+    print('Koszt wyprawy to:')
+    print(koszt)
+
+elif dystans is 0:
+    print('Podałeś o km')
+
+elif dystans is not 0:
+    print('to nie jest 0')
+
+else:
+    print('nie podałeś liczby całowitej')
