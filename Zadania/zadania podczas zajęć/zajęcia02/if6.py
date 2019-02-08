cc6491008955c3fdc138540922d84dc7972a4f5f
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2019-02-08 16:16:17 +0100

    aktualizacja zajęć nr 2

---
+++ Zadania/zadania podczas zajęć/zajęcia02/if6.py
@@ -0,0 +1,9 @@
+liczba = int(input("Podaj liczbę całkowitą: "))
+
+if liczba > 13:
+    print('Wpisana liczba jest większa od 13')
+
+if bool(liczba > 13):  # nie trzeba tak robić, Python robi to za nas
+    print('Wpisana liczba jest większa od 13')
+
+print('podana liczba jest mniejsza od 13')
---
+++ Zadania/zadania podczas zajęć/zajęcia02/if7.py
@@ -0,0 +1,16 @@
+liczba = int(input("Podaj liczbę całkowitą: "))
+
+if liczba > 0 and liczba < 100:
+    print('Liczba jest z zakresu (0, 100)')
+
+# wersja druga
+
+if 0 < liczba < 100:
+    print('Liczba jest z zakresu (0, 100)')
+
+# ctrl + / komentuje cały zaznaczony kod
+
+# oprócz operatora 'and' jest jeszcze 'or' (czyli 'lub')
+
+if 0 < liczba or liczba< 100:
+    print('Liczba jest z zakresu (0, 100)')
