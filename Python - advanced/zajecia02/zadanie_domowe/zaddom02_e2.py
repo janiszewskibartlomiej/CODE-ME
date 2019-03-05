5a5afd22d67cc079ebc4e928e46a3fe4ae03418d
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2019-03-05 20:45:48 +0100

    zajęcia nr 2 - zadanie domowe - aktualizacja

--- Python - advanced/zajecia02/zadanie_domowe/zaddom02_e2.py
+++ Python - advanced/zajecia02/zadanie_domowe/zaddom02_e2.py
@@ -6,25 +6,21 @@ from zaddom02_e1 import TaliaKart
 def rozdaj_karty(talia, ile_kart=1):
     lista_kart = []
 
-    if ile_kart <= 0:
+    # if ile_kart <= 0:
+    #     lista_kart
+    #
+    # if ile_kart == 1:
+    #     lista_kart.append(talia)
+
+    # if ile_kart > 1:
+
+    try:
+        for element in range(ile_kart):
+
+            lista_kart.append(next(talia))
+
+    except StopIteration:
         lista_kart
-
-    if ile_kart == 1:
-        lista_kart.append(talia)
-
-    # if 53 > ile_kart > 1:
-    #     for element in range(ile_kart):
-    #         lista_kart.append(talia)
-
-    if ile_kart > 1:
-
-        try:
-            for element in range(ile_kart):
-
-                lista_kart.append(next(talia))
-
-        except StopIteration:
-            lista_kart
 
     return lista_kart
 
