e5f5507709a365c90ac923d68fb8ab3f91d61902
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2019-03-06 23:14:05 +0100

    zajęcia nr 3 - aktualizacja zadań

--- Python - advanced/zajecia03/zadanie0302/zadanie0302_e1.py
+++ Python - advanced/zajecia03/zadanie0302/zadanie0302_e1.py
@@ -3,4 +3,7 @@
 liczby = [74.59, 52.812, 41.329, 23.323, 78.828, 74.6, 49.828, 3.685, 88.161, 62.02]
 
 zaokraglone = [round(liczba,1) for liczba in liczby]  # tutaj wypełnij
-print(zaokraglone)
+
+if __name__ == '__main__':
+
+    print(zaokraglone)
--- Python - advanced/zajecia03/zadanie0302/zadanie0302_e3.py
+++ Python - advanced/zajecia03/zadanie0302/zadanie0302_e3.py
@@ -1,8 +1,11 @@
 # etap 3
-
+from zadanie0302_e1 import zaokraglone
 
 liczby = [74.59, 52.812, 41.329, 23.323, 78.828, 74.6, 49.828, 3.685, 88.161, 62.02]
 
 
 polskie = [(str(element)).replace('.',',') for element in liczby] # tutaj wypełnij
+polskie_z_e1 = [(str(element)).replace('.',',') for element in zaokraglone] # tutaj wypełnij
 print(polskie)
+print()
+print(polskie_z_e1)
