4b75dc78608d171756c519177e9ebc49882e117e
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2019-03-09 23:44:57 +0100

    zajęcia nr 3 - aktualizacja

--- Python - advanced/zajecia03/zadanie0302/zadanie0302_e4.py
+++ Python - advanced/zajecia03/zadanie0302/zadanie0302_e4.py
@@ -1,10 +1,12 @@
 # etap 4
 
 from random import uniform
+#
+# lista_liczb = []
+# for x in range(10):
+#     x = round(uniform(0, 100), 3)
+#     lista_liczb.append(x)
 
-lista_liczb = []
-for x in range(10):
-    x = round(uniform(0, 100), 3)
-    lista_liczb.append(x)
+lista_liczb = [round(uniform(0, 100), 3) for x in range(10)]
 
 print(lista_liczb)
