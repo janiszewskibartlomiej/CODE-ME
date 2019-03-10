9f3df10265d8eaa6475356c193004fe2d98a48fa
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2019-03-10 10:46:33 +0100

    zajęcia nr 3 - zadane domowe - aktualizacja

--- Python - advanced/zajecia03/zadanie_domowe/zaddom03_e1.py
+++ Python - advanced/zajecia03/zadanie_domowe/zaddom03_e1.py
@@ -4,8 +4,7 @@ liczby = [91111, -7108079251, 405883740, 7942725602, 345220, 2915371507, 30, 943
 
 
 def ile_cyfr(liczby):
-    liczby_rm_minus = [rm for rm in liczby if rm >=0]
-    points = [str(element) for element in liczby_rm_minus]
+    points = [str(element) for element in liczby if element>=0]
     string_with_len = [i +' ({} cyfr)'.format(len(i)) for i in points]
     return string_with_len
 
