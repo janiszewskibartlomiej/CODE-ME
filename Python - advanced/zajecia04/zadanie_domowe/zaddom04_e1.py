a0430d535fa8b6a1cace256c98472f0e1cb732a4
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2019-03-17 18:22:03 +0100

    zajęcia nr 4 - zad domowe

--- Python - advanced/zajecia04/zadanie_domowe/zaddom04_e1.py
+++ Python - advanced/zajecia04/zadanie_domowe/zaddom04_e1.py
@@ -5,33 +5,23 @@ from datetime import datetime
 import random
 
 
-class Timer:
-    def __enter__(self):
-        self.t = datetime.now()
-        timer(przykladowa_funkcja())
-        return self.t
-
-    def __exit__(self, *agr):
-        self.n = datetime.now()
-        delta = self.n - self.t
-        return  print(f'Czas wykonania: {delta}')
-
-
-
 def timer(funkcja):
-    return
-
+    pass
 
 
 def przykladowa_funkcja():
+    t = datetime.now()
+    print('\n',t)
     for _ in range(10):
         print('.', end='', flush=True)
         time.sleep(random.uniform(0, 1))
-    print()
+    n = datetime.now()
+    print('\n',n)
+    delta = n - t
+    print(f'\nCzas wykonania: {delta}')
 
 
 if __name__ == '__main__':
-    with Timer() as t:
-        print()
+    timer(przykladowa_funkcja())
 
 
