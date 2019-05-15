d577467bba50ea6a5fbf4458d928ce58bc882ac9
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2019-05-16 00:44:11 +0200

    modification

--- Python - advanced/The_Form_Project/formSQL.py
+++ Python - advanced/The_Form_Project/formSQL.py
@@ -4,7 +4,7 @@ import sqlite3
 import time
 import hashlib
 
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, get_flashed_messages, flash
 
 app = Flask(__name__)
 
@@ -12,46 +12,67 @@ with open('session_key.txt', 'r') as f:
     key = f.readline()
 app.secret_key = key
 
-@app.route('/', methods=['GET', 'POST'])
-def log_in():
+
+def get_connection():
+    conn = sqlite3.connect('questionDataBase.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+@app.route('/')
+def index():
     if not session:
-        return render_template('log_in.html')
+        return redirect('/login')
 
     id = session.get('user_id')
     user = session.get('user')
     return redirect('/formularz')
 
 
-@app.route('/formularz', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
+def log_in():
+    if request.method == 'GET':
+        messages = get_flashed_messages()
+        return render_template('log_in.html', messages=messages)
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        conn = get_connection()
+        c = conn.cursor()
+
+        print(username)
+        print(password)
+        password = password.encode()
+
+        password = hashlib.sha256(password)
+        password = password.digest()
+
+        zapytanie_password = """
+            SELECT id, user, password FROM "login" WHERE user = ?;
+            """
+        c.execute(zapytanie_password, (username,))
+        line_from_base = c.fetchone()
+
+        print('hasła:', password, list(line_from_base))
+
+        if line_from_base[2] and password == line_from_base[2]:
+            session['user_id'] = line_from_base[0]
+            session['user'] = line_from_base[1]
+
+            return redirect('/formularz')
+
+        flash('błędna nazwa użytkownika lub hasło')
+        return redirect('/login')
+
+
+@app.route('/formularz', methods=['GET', 'POST'])
 def formularz():
-    conn = sqlite3.connect('questionDataBase.db')
-    c = conn.cursor()
-    username = request.form['username']
-
-    password = request.form['password']
-    print(username)
-    print(password)
-    password = password.encode()
-
-    password = hashlib.sha256(password)
-    password = password.digest()
-
-    zapytanie_password = """
-    SELECT id, user, password FROM "login" WHERE user = ?;
-    """
-    c.execute(zapytanie_password, (username,))
-    line_from_base = c.fetchone()
-
-    print('hasła:', password, line_from_base)
-
-    if line_from_base[2] and password == line_from_base[2]:
-        session['user_id'] = line_from_base[0]
-        session['user'] = line_from_base[1]
-
+    if session:
         return render_template('input_question.html')
 
-    else:
-        return redirect('/')
+    return redirect('/')
 
 
 @app.route('/dodaj', methods=['GET', 'POST'])
@@ -114,6 +135,13 @@ def delete():
     conn.commit()
 
     return redirect('/baza')
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+
+    return redirect('/')
 
 
 if __name__ == "__main__":
